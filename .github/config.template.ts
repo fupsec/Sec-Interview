3c8634385703d94bfbae0f47dcdb9fc4ec5758a9
雾島风起時 <suc2es2.clc@gmail.com>
2026-01-02 01:19:56 +0800

    Delete .github directory

--- .github/config.template.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitepress';
-import sidebar from './sidebar.mjs';
-
-export default defineConfig({
-  title: 'Sec-Interview',
-  description: '安全面试笔记',
-  base: '/Sec-Interview/',
-  themeConfig: { sidebar }
-});
